379970bd64c85bbf478eb11f7bc2bce3024e7d15
Logan Watkins <lw2014wa@gmail.com>
2023-05-04 21:14:47 -0500

    Add expressionTree.py

---
+++ expressionTree.py
@@ -0,0 +1,18 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def evaluateExpressionTree(tree):
+    if tree.value == -1:
+        return evaluateExpressionTree(tree.left) + evaluateExpressionTree(tree.right)
+    if tree.value == -2:
+        return evaluateExpressionTree(tree.left) - evaluateExpressionTree(tree.right)
+    if tree.value == -3:
+        return int(evaluateExpressionTree(tree.left) / evaluateExpressionTree(tree.right))
+    if tree.value == -4:
+        return evaluateExpressionTree(tree.left) * evaluateExpressionTree(tree.right)
+    return tree.value
