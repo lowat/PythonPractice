0d844f5d22d25583fde78f397fd87141b5e0c16f
Logan Watkins <lw2014wa@gmail.com>
2023-09-08 17:14:38 -0500

    Add arrProduct.py

---
+++ arrProduct.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1 for num in nums]
+
+        for i in range(1, len(nums)):
+            result[i] = nums[i - 1] * result[i - 1]
+
+        postfix = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+
+        return result
