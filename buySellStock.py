9a5dff8ba6ba5326ac2f115fc5b3c9c011a0962c
Logan Watkins <lw2014wa@gmail.com>
2023-09-19 21:10:37 -0500

    Add buySellStock.py

--- buySellStock.py
+++ buySellStock.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def maxProfit(self, prices):
-        lowest = prices[0]
-        profit = 0
+        lowest, profit = prices[0], 0
         for price in prices[1:]:
             if price < lowest:
                 lowest = price
-                continue
             else:
                 profit = max(profit, price - lowest)
         return profit
